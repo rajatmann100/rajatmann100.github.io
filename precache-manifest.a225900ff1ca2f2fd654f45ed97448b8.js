49b952673b32e021e28d40b785d41c797a7b665b
Rajat Mann <rajatmann100@gmail.com>
2020-01-09 19:07:41 +0530

    first build

---
+++ precache-manifest.a225900ff1ca2f2fd654f45ed97448b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3dcbf923ba26ab6978be53a79a38110",
+    "url": "/index.html"
+  },
+  {
+    "revision": "512e18f7f40f563678ff",
+    "url": "/static/css/main.7374d94b.chunk.css"
+  },
+  {
+    "revision": "69a5faa7706abf46af2e",
+    "url": "/static/js/2.7bd15741.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.7bd15741.chunk.js.LICENSE"
+  },
+  {
+    "revision": "512e18f7f40f563678ff",
+    "url": "/static/js/main.fab66fa0.chunk.js"
+  },
+  {
+    "revision": "895d0b23d435123497fc",
+    "url": "/static/js/runtime-main.9c4624c2.js"
+  }
+]);
