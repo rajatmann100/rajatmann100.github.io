6c25487efa1f35303babdc6d98d48f96af6539b9
Rajat mann <rajatmann@Rajats-MacBook-Air.local>
2020-08-20 16:58:02 +0530

    covid us carousel

---
+++ precache-manifest.aade13a7320dcc6e19e5e3322e93da4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c715800f45713fdb0f43595b7da604f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f620e789935f56a149b",
+    "url": "/static/css/2.5eb6b162.chunk.css"
+  },
+  {
+    "revision": "63c2472d32e81020de91",
+    "url": "/static/css/main.649880c7.chunk.css"
+  },
+  {
+    "revision": "1f620e789935f56a149b",
+    "url": "/static/js/2.f2a3a628.chunk.js"
+  },
+  {
+    "revision": "04d8406652b56c3c1c3cf354d459285a",
+    "url": "/static/js/2.f2a3a628.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63c2472d32e81020de91",
+    "url": "/static/js/main.3d3bf095.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
